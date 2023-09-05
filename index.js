4eea52089636fc9bfd26cb110b59b98c2497a46a
Marko Pavleka <markopavleka@gmx.at>
2023-09-05 18:08:03 +0200

    update the code

--- index.js
+++ index.js
@@ -3,19 +3,13 @@ import randomColor from 'randomcolor';
 
 const color = randomColor(); // a hex code for an attractive color
 
-const height = 9;
-const width = 31;
-
-/*for (let i = 0; i <= height; i++) {
-  const row = '#'.repeat(width);
-  console.log(row);
-}*/
-
 console.log(chalk.hex(color)`###############################`);
 console.log(chalk.hex(color)`###############################`);
 console.log(chalk.hex(color)`####                       ####`);
 console.log(
-  chalk.hex(color)`####        ` + color + chalk.hex(color)`        ####`,
+  chalk.hex(color)`####        ` +
+    chalk.hex(color)(`${color}`) +
+    chalk.hex(color)`        ####`,
 );
 console.log(chalk.hex(color)`####                       ####`);
 console.log(chalk.hex(color)`###############################`);
